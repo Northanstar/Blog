270df80c1d930970a1440f722cecff6bfd99c1e7
Zeek <chigereisaac31@gmail.com>
2025-05-09 12:38:02 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import { useState } from 'react'; 
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
 import Register from './Pages/Register';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './Pages/Login';
 import Reset from './Pages/Reset';
+import { auth } from '../src/Firebaseconfig';
+import { signOut } from "firebase/auth";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
+
 
 const App = () => {
+
+  const navigate = useNavigate();
+
+  const [isAuth, setIsAuth] = useState(localStorage.getItem('isAuth'));
+
   return (
     <div>
+      <ToastContainer position='top-right' theme="colored" />
+      <Navbar isAuth={isAuth}  />
+
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home isAuth={isAuth} />} />
+        <Route path='/register' element={<Register setIsAuth={setIsAuth} />} />
+        <Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
         <Route path='/reset' element={<Reset />} />
        </Routes>    
     </div>
